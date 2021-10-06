034ab760e1d743c6614dc777a501440b25362746
YuMeow <yuzh19@tsinghua.org.cn>
2021-10-06 15:57:19 +0800

    把工具箱函数 normrnd 改成了主程序自带的 randn

--- channel_trans.m
+++ channel_trans.m
@@ -4,10 +4,10 @@ x = [x,0];
 len=length(x);
 beta=zeros(1,len);
 y=zeros(1,len);
-z=normrnd(0,sqrt(0.5),[1,len])+1j*normrnd(0,sqrt(0.5),[1,len]);
-n=normrnd(0,sigma/sqrt(2),[1,len])+1j*normrnd(0,sigma/sqrt(2),[1,len]);
+z=sqrt(0.5)*randn(1,len)+1j*sqrt(0.5)*randn(1,len);
+n=sigma/sqrt(2)*randn(1,len)+1j*sigma/sqrt(2)*randn(1,len);
 
-beta(1)=normrnd(0,sqrt(0.5))+1j*normrnd(0,sqrt(0.5));
+beta(1)=sqrt(0.5)*randn(1)+1j*sqrt(0.5)*randn(1);
 y(1)=sqrt(1-b^2)*x(1)+n(1);
 for i=2:len
     beta(i)=rho*beta(i-1)+sqrt(1-rho^2)*z(i);
