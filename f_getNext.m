11642771e2d0a2e597cec625ece9029195b00309
yxj <564197835@qq.com>
2021-10-06 18:45:50 +0800

    更新getNext.m中A系数矩阵为(k,n,m)

--- f_getNext.m
+++ f_getNext.m
@@ -1,19 +1,23 @@
 %% 计算 (n,k,m) 卷积码的下一个状态和输出
 % m - 延时拍数加1。不延时值为1
-% A - 卷积码参数。A(i,:,:) 是 k 行 n 列矩阵，i = 1:m
-% currentState - 长度为 (m-1)k 的行向量
-% nextState - 长度为 (m-1)k 的行向量
-% input - 长度为 k 的行向量
+% A - 卷积码参数。A(k,n,m)
+% currentState - 长度为 (m-1)k 的行向量 (一种状态长度为k 有m-1种之前的状态)
+% nextState - 长度为 (m-1)k 的行向量 (计算下一次状态)
+% input - 长度为 k 的行向量 (需要和当前状态拼接得到完整状态)
 % output - 长度为 n 的行向量
 function [nextState, output] = f_getNext(n, k, m, A, currentState, input)
-    assert(all(size(input) == [1, k], "input 应为长度为 k 的行向量"))
-    assert(all(size(A) == [m, k, n], "size(A) 应为[m, k, n]"))
-    
-    fullState = reshape([currentState, input], k, m)'; % m 行 k 列
-    nextState = [currentState(k+1:end), input]; % 长度为 (m-1)k 的行向量
-    
+    assert(all(size(input) == [1, k]), "input 应为长度为 k 的行向量")
+    assert(all(size(A) == [k, n, m]), "size(A) 应为[k,n,m]")
+
+    fullState = [currentState, input]; % 长度为 m*k 的行向量 最新加入的状态在最右边 这是课件上的状态转换方法
+    nextState = fullState((k + 1):(m * k)); % 长度为 (m-1)*k 的行向量
+
     output = zeros(1, n);
-    for mm = 1:m
-        output = output + fullState(mm, :) * A(mm, :, :);
+
+    % A作用于x求和
+    for i = 1:m
+        output = output + fullState(i) * A(:, :, i); % 1*k k*n
+        output = mod(output, 2);
     end
+
 end
---
+++ test_getNext.m
@@ -0,0 +1,17 @@
+clear,close,clc;
+
+n = 2; k = 1; m = 3;
+A = cat(3, [1 1], [0 1], [1 1]); % A_2 ~ A_0的拼接 A2=[1 1] A1=[0 1] A0=[1 1]
+r = [1 1 0 1 1 0 1 1 0 0];
+
+currentState = [0 0];
+input = 1;
+[nextState, output] = f_getNext(n, k, m, A, currentState, input) % 应该是 01 11
+
+currentState = [1 0];
+input = 0;
+[nextState, output] = f_getNext(n, k, m, A, currentState, input) % 应该是 00 11
+
+currentState = [1 1];
+input = 1;
+[nextState, output] = f_getNext(n, k, m, A, currentState, input) % 应该是 11 01
