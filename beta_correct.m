ee23421a4b076681ff70b3622b5e33de0795dd74
YuMeow <yuzh19@tsinghua.org.cn>
2021-10-06 21:17:43 +0800

    调整了 beta_correct

--- beta_correct.m
+++ beta_correct.m
@@ -1,24 +1,22 @@
-%% 收方利用已知的 beta 对收到的复向量 y 进行校正，得到复向量 x 作为收到
+%% 收方利用已知的 beta 对收到的复向量 r 进行校正，得到复向量 x 作为收到
 % 的信号。
-function x = beta_correct(y, b, beta)
-    % y - 行向量，表示收到的复向量
+function x = beta_correct(r, b, beta, beta_corr_mode)
+    % r - 行向量，表示收到的复向量
     % b - 标量，为题目中的参数
-    % beta - 行向量，为题目中的参数。长度与 y 相同。
-    % x - 行向量。长度为 length(y) 或 length(y)-1
-    % 作者：于子涵
-    % 更新：2021/10/5
+    % beta - 行向量，为题目中的参数。长度与 r 相同。
+    % x - 行向量。长度为 length(r)
 
-    if (isempty(beta))
-        x = y;
+    if (beta_corr_mode == 3)
+        x = r;
     elseif (b == 1)
-        t = y ./ beta;
-        x = t(2:end);
+        t = r ./ beta;
+        x = [t(2:end), 0];
     else
-        x = zeros(1, length(y) - 1);
-        x(1) = y(1) / sqrt(1 - b^2);
+        x = zeros(size(r));
+        x(1) = r(1) / sqrt(1 - b^2);
 
         for i = 2:length(x)
-            x(i) = (y(i) - b * beta(i) * x(i - 1)) / sqrt(1 - b^2);
+            x(i) = (r(i) - b * beta(i) * x(i - 1)) / sqrt(1 - b^2);
         end
 
     end
