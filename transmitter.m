7dab9c6cbd5f958130032507ce3348991b002a45
YuMeow <yuzh19@tsinghua.org.cn>
2021-10-06 21:19:42 +0800

    修改了 transmitter

--- transmitter.m
+++ transmitter.m
@@ -1,29 +1,8 @@
-%% notes
-% message: input 01 array
-% modulation: modulation mode(1,2,3)
-% r: modulation radius
-% (n, k, m), A: conv parameters
-% len : CRC poly length
-%% message conv code utlization
-% message self implemetation (add head/tail,add CRC?)
-function [y, n, k, m, A] = transmitter(x, modulation, r, n, k, m, A)
-
-    %% CONV
-    x = conv_encoding(n, k, m, A, x, 2);
-
-    %% MODULATION
-    % message constellation mapping (PSK modulation)
-    % easy to change to QAM modulation
-    if (modulation == 1) %BPSK
-        mapping_2 = [-r, r];
-        y = mapping_2(x + 1);
-    elseif (modulation == 2) %QPSK
-        mapping_4 = r * exp(1j * pi * [1 3 7 5] / 4); %Gray code constraint
-        x = [x, zeros(1, mod(-length(x), 2))];
-        y = mapping_4(bin2dec(char(reshape(x, 2, [])' + '0'))' + 1);
-    elseif (modulation == 3)
-        mapping_8 = r * exp(1j * pi * [0 1 3 2 7 6 4 5] / 4);
-        x = [x, zeros(1, mod(-length(x), 3))];
-        y = mapping_8(bin2dec(char(reshape(x, 3, [])' + '0'))' + 1);
-    end
+%% MODULATION
+% x: input 01 array
+% N: 1(mapping_2) or 2(mapping_4) or 3(mapping_8)
+% mapping: mapping_2 or mapping_4 or mapping_8
+function y = transmitter(x, N, mapping)
+    x = [x, zeros(1, mod(-length(x), N))];
+    y = mapping(bin2dec(char(reshape(x, N, [])' + '0'))' + 1);
 end
