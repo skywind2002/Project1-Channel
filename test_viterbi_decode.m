781a4b6c0f50bfcd7bc299faf132213059cf5daf
yxj <564197835@qq.com>
2021-10-06 19:00:38 +0800

    add comments in test file

--- test_viterbi_decode.m
+++ test_viterbi_decode.m
@@ -4,4 +4,4 @@ n = 2; k = 1; m = 3;
 A = cat(3, [1 1], [0 1], [1 1]); % A_2 ~ A_0的拼接 A2=[1 1] A1=[0 1] A0=[1 1]
 r = [1 1 0 1 1 0 1 1 0 0];
 
-[decodedResult, minError] = viterbi_decode(n, k, m, A, r)
+[decodedResult, minError] = viterbi_decode(n, k, m, A, r) % 应该得到 11000 2 这里译码结果为收尾的情况 去掉最后两个0 得到 110
