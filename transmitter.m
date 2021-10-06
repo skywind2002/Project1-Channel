96664cecb891f6700e9f00bdf3a245b09e3c423e
YuMeow <yuzh19@tsinghua.org.cn>
2021-10-06 18:50:49 +0800

    将 conv_coding 函数并入了 transmitter 中； 将 CRC 部分放在了 transmitter 之外； 大幅度修改了main

--- transmitter.m
+++ transmitter.m
@@ -2,37 +2,34 @@
 %message: input 01 array
 %modulation: modulation mode(1,2,3)
 %r: modulation radius
-%conv: conv code mode(2,3)
+% (n, k, m), A: conv parameters
 %len : CRC poly length
 %% message conv code utlization
 %message self implemetation (add head/tail,add CRC?)
-function [y]=transmitter(message,modulation,r,conv,len,crc_ENB)
-% CRC
-if(crc_ENB==1)
-    message=CRC(message,len);
+function [y, n, k, m, A] = transmitter(x, modulation, r, n, k, m, A)
+
+%% CONV
+x = [zeros(1, m-1), x]; % 前面补零，从零状态开始
+x = [x, zeros(1, mod(-length(x), k))]; % 后面补零，使得 x 长度为 k 的整数倍
+A = reshape(permute(A, [3, 1, 2]), m*k, n); % 化为 m*k 行 n 列矩阵方便后续计算
+y = zeros(length(x)/k-m+1, n);
+for t = m:length(x)/k
+    y(t-m+1,:) = mod(double(x((t-m)*k+1:t*k)) * A, 2);
 end
-% conv
-if(conv == 2)
-    A = [1 1 0 1; 1 1 1 1]; % size = [n, m, (k)]
-    A = permute(A, [3, 1, 2]); % size = [k, n, m]
-    x = conv_coding(2, 1, 4, A, message);
-elseif(conv == 3)
-    A = [1 0 1 1; 1 1 0 1; 1 1 1 1];
-    A = permute(A, [3, 1, 2]); % size = [k, n, m]
-    x = conv_coding(3, 1, 4, A, message);
-end
-% modulation
-%% message constellation mapping (PSK modulation)
+y = reshape(y', 1, []);
+
+%% MODULATION
+% message constellation mapping (PSK modulation)
 % easy to change to QAM modulation
-mapping_2 = [-r, r];
-mapping_4 = r * exp(1j * pi * [1 3 7 5] / 4); %Gray code constraint
-mapping_8 = r * exp(1j * pi * [0 1 3 2 7 6 4 5] / 4);
 if (modulation == 1)  %BPSK
+    mapping_2 = [-r, r];
     y = mapping_2(x + 1);
 elseif (modulation == 2) %QPSK
+    mapping_4 = r * exp(1j * pi * [1 3 7 5] / 4); %Gray code constraint
     x = [x, zeros(1, mod(-length(x), 2))];
     y = mapping_4(bin2dec(char(reshape(x, 2, [])'+'0'))' + 1);
 elseif (modulation == 3)
+    mapping_8 = r * exp(1j * pi * [0 1 3 2 7 6 4 5] / 4);
     x = [x, zeros(1, mod(-length(x), 3))];
     y = mapping_8(bin2dec(char(reshape(x, 3, [])'+'0'))' + 1);
 end
