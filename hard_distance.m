1528032289e5def2188e5b9c795e5592f9d85131
YuMeow <yuzh19@tsinghua.org.cn>
2021-10-12 18:54:59 +0800

    完成了 soft Viterbi，感觉好像稳定一些，对10000个点能跑出0.05的BER

--- hard_distance.m
+++ hard_distance.m
@@ -1,5 +1,5 @@
-% 判断行向量 a 原来其实是行向量 b 的概率
-function p = hard_distance(a, b, p)
-    d = mod(a - b, p);
-    p = 0.1 .^ sum(d, 2);
+% 判断实际的行向量 a 与理想的行向量 b 的距离
+function d = hard_distance(a, b, p)
+    % d = mod(a - b, p);
+    d = sum(a ~= b, 2);
 end
--- soft_distance.m
+++ soft_distance.m
@@ -1,7 +1,7 @@
-% 判断复数 z 来自于调制前行向量 y 的概率
-function probability = soft_distance(z, y, mapping, p)
+% 判断复数 z 与调制前行向量 y 的距离
+function d = soft_distance(z, y, mapping, p)
     b = base2dec(char(y + '0'), p) + 1;
-    m = mapping(b)';
-    d = abs(m - z);
-    probability = prod(exp(-d), 2);
+    m = mapping(b).';
+    dis = log(abs(m - z) + 1);
+    d = sum(dis, 2);
 end
